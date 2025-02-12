51a5c5d71ab98ba95303e25186a6343078c0b7b8
muxazzXJohny <maracoco334@gmail.com>
2025-02-12 09:25:02 +0200

    Add files via upload

---
+++ ConsoleApp12.02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12._02
+{
+    class Program
+    {
+        //Выполнить инициализацию одномерного массива с использованием конструктора без указания значений элементов и вывести на экран.
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            int[] omas = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"omas[{i}]=");
+                omas[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.WriteLine($"omas[{i}]={omas[i]}");
+            }
+            Console.ReadKey();
+;
+        }
+    }
+}
